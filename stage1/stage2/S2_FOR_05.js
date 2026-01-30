db00f8e0b185678ca6d3603ce9953857f4f2ed22
Zloy100 <osmcalasa@gmail.com>
2026-01-30 18:48:53 +0100

    Create S2_FOR_05.js

---
+++ stage1/stage2/S2_FOR_05.js
@@ -0,0 +1,11 @@
+function printMultiplicationTable() {
+  for (let i = 1; i <= 10; i++) {
+    let row = "";
+    for (let j = 1; j <= 10; j++) {
+      row += (i * j).toString().padStart(4);
+    }
+    console.log(row);
+  }
+}
+
+printMultiplicationTable();
