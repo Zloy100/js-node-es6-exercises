c1aceac7f6ac575c2861722960d4b71251987dc1
Zloy100 <osmcalasa@gmail.com>
2026-01-30 18:54:07 +0100

    Create S3_FN_01.js

---
+++ stage1/stage3/S3_FN_01.js
@@ -0,0 +1,8 @@
+// S3_FN_01.js
+const square = n => n * n;
+const isOdd = n => n % 2 !== 0;
+const greet = name => `Hello, ${name}!`;
+
+console.log(square(7));          // 49
+console.log(isOdd(10));          // false
+console.log(greet("Sara"));      // Hello, Sara!
